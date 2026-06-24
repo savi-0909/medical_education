a7dfd2d0e2e2823793e7008a25c9a8e7bd058258
Savi0409 <165547567+savi-0909@users.noreply.github.com>
2026-06-24 11:59:38 +0530

    Update ems_master.sql

--- DB Files/ems_master.sql
+++ DB Files/ems_master.sql
@@ -1,8 +1,30 @@
 CREATE DATABASE ems_dev;
 use ems_dev;
 
-DROP TABLE IF EXISTS tbl_region_master;
 
+-- ============================================================================
+--  MEDICAL EDUCATION MASTER TABLES
+-- ============================================================================
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    inst_id INT NULL,
+    password VARCHAR(255) DEFAULT NULL,
+    department VARCHAR(100) DEFAULT NULL,
+    created_by VARCHAR(100) DEFAULT NULL,
+    role VARCHAR(50) NOT NULL DEFAULT 'user',
+    permissions_json TEXT,
+    PRIMARY KEY (id),
+    UNIQUE KEY username (username),
+    CONSTRAINT fk_users_inst
+        FOREIGN KEY (inst_id)
+        REFERENCES tbl_inst_master(inst_id)
+);
+
+
+DROP TABLE IF EXISTS tbl_region_master;
 CREATE TABLE tbl_region_master (
     region_id INT PRIMARY KEY CHECK (region_id BETWEEN 0 AND 99),
     region_desc VARCHAR(200),
@@ -14,7 +36,6 @@ CREATE TABLE tbl_region_master (
 );
 
 DROP TABLE IF EXISTS tbl_category_master;
-
 CREATE TABLE tbl_category_master (
     cat_id INT PRIMARY KEY CHECK (cat_id BETWEEN 0 AND 99999),
     cat_desc VARCHAR(100),
@@ -27,7 +48,6 @@ CREATE TABLE tbl_category_master (
 
 
 DROP TABLE IF EXISTS tbl_year_id;
-
 CREATE TABLE tbl_year_id (
     year_id INT PRIMARY KEY CHECK (year_id BETWEEN 0 AND 99),
     year_desc VARCHAR(100),
@@ -51,7 +71,6 @@ CREATE TABLE tbl_month_master (
 );
 
 DROP TABLE IF EXISTS tbl_exam_sem_master;
-
 CREATE TABLE tbl_exam_sem_master (
     sem_id INT PRIMARY KEY CHECK (sem_id BETWEEN 0 AND 99),
     sem_desc VARCHAR(200),
@@ -62,9 +81,12 @@ CREATE TABLE tbl_exam_sem_master (
     updated_by VARCHAR(50),
     updated_date DATETIME,
     status_ BOOLEAN,
-
-    FOREIGN KEY (start_month) REFERENCES tbl_month_master(month_id),
-    FOREIGN KEY (end_month) REFERENCES tbl_month_master(month_id)
+    CONSTRAINT fk_exam_sem_start_month
+        FOREIGN KEY (start_month)
+        REFERENCES tbl_month_master(month_id),
+    CONSTRAINT fk_exam_sem_end_month
+        FOREIGN KEY (end_month)
+        REFERENCES tbl_month_master(month_id)
 );
 
 
@@ -82,11 +104,9 @@ CREATE TABLE tbl_inst_master (
     updated_by VARCHAR(50),
     updated_date DATETIME,
     status_ BOOLEAN,
-
     CONSTRAINT fk_inst_region
         FOREIGN KEY (region_id)
         REFERENCES tbl_region_master(region_id),
-
     CONSTRAINT fk_inst_category
         FOREIGN KEY (cat_id)
         REFERENCES tbl_category_master(cat_id)
@@ -94,7 +114,6 @@ CREATE TABLE tbl_inst_master (
 
 
 DROP TABLE IF EXISTS tbl_course_master;
-
 CREATE TABLE tbl_course_master (
     course_id INT PRIMARY KEY CHECK (course_id BETWEEN 0 AND 99999),
     course_desc VARCHAR(200),
@@ -110,7 +129,6 @@ CREATE TABLE tbl_course_master (
 
 
 DROP TABLE IF EXISTS tbl_subject_master;
-
 CREATE TABLE tbl_subject_master (
     subject_id INT PRIMARY KEY CHECK (subject_id BETWEEN 0 AND 9999),
     subject_desc VARCHAR(200),
@@ -123,78 +141,57 @@ CREATE TABLE tbl_subject_master (
     status_ BOOLEAN
 );
 
-
+-- ============================================================================
+--  MEDICAL EDUCATION TRANSACTION TABLES
+-- ============================================================================
 
 DROP TABLE IF EXISTS tbl_inst_course_map;
-
 CREATE TABLE tbl_inst_course_map (
     inst_course_id INT PRIMARY KEY CHECK (inst_course_id BETWEEN 0 AND 9999999999),
     inst_id INT NOT NULL,
     course_id INT NOT NULL,
-    created_by VARCHAR(50) ,
-    created_date DATETIME,
-    updated_by VARCHAR(50),
-    updated_date DATETIME,
-    status_ BOOLEAN,
-
+    created_by VARCHAR(50) DEFAULT NULL,
+    created_date DATETIME DEFAULT NULL,
+    updated_by VARCHAR(50) DEFAULT NULL,
+    updated_date DATETIME DEFAULT NULL,
+    status_ BOOLEAN DEFAULT NULL,
     CONSTRAINT fk_inst_course_map_inst
         FOREIGN KEY (inst_id)
         REFERENCES tbl_inst_master(inst_id),
-
     CONSTRAINT fk_inst_course_map_course
         FOREIGN KEY (course_id)
         REFERENCES tbl_course_master(course_id),
-
     CONSTRAINT uq_inst_course_map
         UNIQUE (inst_id, course_id)
 );
 
 
 DROP TABLE IF EXISTS tbl_course_subject_map;
-
 CREATE TABLE tbl_course_subject_map (
     course_subject_id INT PRIMARY KEY CHECK (course_subject_id BETWEEN 0 AND 9999999999),
     course_id INT NOT NULL,
     subject_id INT NOT NULL,
     year_id INT NOT NULL,
     sem_id INT NOT NULL,
-    priority_id INT,
+    priority_id INT NOT NULL,
     created_by VARCHAR(50) DEFAULT 'admin',
-    created_date DATETIME,
-    updated_by VARCHAR(50),
-    updated_date DATETIME,
-    status_ BOOLEAN,
-
+    created_date DATETIME DEFAULT NULL,
+    updated_by VARCHAR(50) DEFAULT NULL,
+    updated_date DATETIME DEFAULT NULL,
+    status_ BOOLEAN DEFAULT NULL,
     CONSTRAINT fk_course_subject_map_course
         FOREIGN KEY (course_id)
         REFERENCES tbl_course_master(course_id),
-
     CONSTRAINT fk_course_subject_map_subject
         FOREIGN KEY (subject_id)
         REFERENCES tbl_subject_master(subject_id),
-
     CONSTRAINT fk_course_subject_map_year
         FOREIGN KEY (year_id)
         REFERENCES tbl_year_id(year_id),
-
     CONSTRAINT fk_course_subject_map_sem
         FOREIGN KEY (sem_id)
         REFERENCES tbl_exam_sem_master(sem_id),
-
     CONSTRAINT uq_course_subject_map
         UNIQUE (course_id, subject_id, year_id, sem_id)
 );
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
-  username varchar(50) NOT NULL,
-  password varchar(255) DEFAULT NULL,
-  department varchar(100) DEFAULT NULL,
-  created_by varchar(100) DEFAULT NULL,
-  role varchar(50) NOT NULL DEFAULT 'user',
-  permissions_json text,
-  PRIMARY KEY (id),
-  UNIQUE KEY username (username)
-);
